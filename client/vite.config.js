1b9513c16bf4a6fd589a1a91125ffecd6fb8ff48
gavinlikewind <gavinlikewind@gmail.com>
2024-03-27 09:42:52 -0400

    no message

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const { PORT = 3000} = process.env;
+const { PORT = 10000 } = process.env;
 export default defineConfig({
 	plugins: [react()],
 	server:{
