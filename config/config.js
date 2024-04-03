1b1c80f986939357288794928671f2d55b795a0d
gavinlikewind <gavinlikewind@gmail.com>
2024-04-03 09:59:48 -0400

    update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
    
     const config = {
         env: process.env.NODE_ENV || 'development', 
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 10000,
         jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
         mongoUri: process.env.MONGODB_URI || "mongodb+srv://gavinlikewind:71HLrYHNxU6VOuB9@cluster0.k7pj1q8.mongodb.net/Skeleton?retryWrites=true&w=majority"||
         process.env.MONGO_HOST ||
